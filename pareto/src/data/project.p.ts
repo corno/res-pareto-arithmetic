0e624dd1677272a275cad04acb997e9a2de215c0
corno <corno.astn.a@schraverus.com>
2023-02-02 10:49:57 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,8 +7,6 @@ const d = pr.wrapRawDictionary
 import { $ as api } from "./api.p"
 
 export const $: mproject.TProject = {
-    'name': "res-pareto-arithmetic",
-
     'author': "Corno",
     'description': "a pareto wrapper for arithmetic operations as pareto lacks them",
     'license': "ISC",
