dca93b4c19cc69e0ecc09027ce6ad9e908da7dc1
corno <corno.astn.a@schraverus.com>
2023-03-13 16:58:41 +0100

    .

--- pareto/src/data/glossary.data.ts
+++ pareto/src/data/glossary.data.ts
@@ -7,19 +7,23 @@ import {
     dictionary, member, taggedUnion, types, group,
     array,
     typeReference,
-    data,
-    func,
+    sdata,
+    sfunc,
     type,
     optional,
-    reference,
     number,
+    imp,
+    externalTypeReference,
 } from "lib-pareto-typescript-project/dist/submodules/glossary/shorthands"
 
-import * as gglossary from "lib-pareto-typescript-project/dist/submodules/glossary"
+import * as g_glossary from "lib-pareto-typescript-project/dist/submodules/glossary"
 const d = pd.d
 
-export const $: gglossary.T.Glossary<pd.SourceLocation> = {
+export const $: g_glossary.T.Glossary<pd.SourceLocation> = {
     'parameters': d({}),
+    'imports': d({
+        "common": imp({}),
+    }),
     'types': d({
         "NumberRange": type(nested(number())),
         "DivisionData": type(group({
@@ -38,29 +42,31 @@ export const $: gglossary.T.Glossary<pd.SourceLocation> = {
         "NumberDictionary": type(dictionary(number())),
         "NumberArray": type(array(number())),
     }),
-    'builders': d({}),
-    'interfaces': d({}),
-    'functions': d({
-        "Add": func(typeReference("NumberRange"), null, null, data(typeReference("common", "Number"), false)),
-        "Multiply": func(typeReference("NumberRange"), null, null, data(typeReference("common", "Number"), false)),
-        "Negate": func(typeReference("common", "Number"), null, null, data(typeReference("common", "Number"), false)),
-        /**
-        * if the denominator is 0, the return value is null
-        * (division by 0),
-        */
-        "DivideWithRemainder": func(typeReference("DivisionData"), null, null, data(typeReference("DivisionResult"), false)),
-        /**
-         * calculates the max of an array of numbers. If there are no entries, the result value is 0
-         */
-        "ArrayMaxOrZero": func(typeReference("NumberArray"), null, null, data(typeReference("common", "Number"), false)),
-        /**
-         * calculates the max of a dictionary of number. If there are no entries, the result value is 0
-         */
-        "DictionaryMaxOrZero": func(typeReference("NumberDictionary"), null, null, data(typeReference("common", "Number"), false)),
-        /**
-        * calculates the max of a range. If there are no entries, the result value is 0
-        */
-        "MaxOrZero": func(typeReference("NumberRange"), null, null, data(typeReference("common", "Number"), false)),
-        "Substract": func(typeReference("SubstractData"), null, null, data(typeReference("common", "Number"), false)),
-    }),
+    'type': ['synchronous', {
+        'builders': d({}),
+        'functions': d({
+            "Add": sfunc(typeReference("NumberRange"), null, null, sdata(externalTypeReference("common", "Number"))),
+            "Multiply": sfunc(typeReference("NumberRange"), null, null, sdata(externalTypeReference("common", "Number"))),
+            "Negate": sfunc(externalTypeReference("common", "Number"), null, null, sdata(externalTypeReference("common", "Number"))),
+            /**
+            * if the denominator is 0, the return value is null
+            * (division by 0),
+            */
+            "DivideWithRemainder": sfunc(typeReference("DivisionData"), null, null, sdata(typeReference("DivisionResult"))),
+            /**
+             * calculates the max of an array of numbers. If there are no entries, the result value is 0
+             */
+            "ArrayMaxOrZero": sfunc(typeReference("NumberArray"), null, null, sdata(externalTypeReference("common", "Number"))),
+            /**
+             * calculates the max of a dictionary of number. If there are no entries, the result value is 0
+             */
+            "DictionaryMaxOrZero": sfunc(typeReference("NumberDictionary"), null, null, sdata(externalTypeReference("common", "Number"))),
+            /**
+            * calculates the max of a range. If there are no entries, the result value is 0
+            */
+            "MaxOrZero": sfunc(typeReference("NumberRange"), null, null, sdata(externalTypeReference("common", "Number"))),
+            "Substract": sfunc(typeReference("SubstractData"), null, null, sdata(externalTypeReference("common", "Number"))),
+        }),
+
+    }],
 }
--- typescript/test/src/modules/main/implementations/getTestSet.p.ts
+++ typescript/test/src/modules/main/implementations/getTestSet.p.ts
@@ -3,28 +3,28 @@ import * as ps from 'pareto-core-state'
 import * as pl from 'pareto-core-lib'
 import * as pa from 'pareto-core-async'
 
-import * as gtest from "lib-pareto-test"
+import * as g_test from "lib-pareto-test"
 
-import * as gpub from "../../../../../pub"
-import * as gbool from "res-pareto-boolean"
+import * as g_pub from "../../../../../pub"
+import * as g_bool from "res-pareto-boolean"
 
 import { getTestSet } from "../api.generated"
 
 export const $$: getTestSet = () => {
 
-    const builder = ps.createUnsafeDictionaryBuilder<gtest.T.TestElement>()
+    const builder = ps.createUnsafeDictionaryBuilder<g_test.T.TestElement>()
     function createTest(name: string, actual: number, expected: number) {
         builder.add(name, {
             type: ['test', {
-                type: ['boolean', gbool.$r.equal({ this: actual, that: expected })]
+                type: ['boolean', g_bool.$r.equal({ this: actual, that: expected })]
             }]
         })
     }
 
-    createTest("add - empty", gpub.$r.add([]), 0)
-    createTest("add - entries", gpub.$r.add([4, 6, 2]), 12)
-    createTest("negative", gpub.$r.negate(2), -2)
-    pl.cc(gpub.$r.divideWithRemainder({ numerator: 7, denominator: 2 }), ($) => {
+    createTest("add - empty", g_pub.$r.add([]), 0)
+    createTest("add - entries", g_pub.$r.add([4, 6, 2]), 12)
+    createTest("negative", g_pub.$r.negate(2), -2)
+    pl.cc(g_pub.$r.divideWithRemainder({ numerator: 7, denominator: 2 }), ($) => {
         if ($[0] === true) {
 
             createTest("quotient", $[1].quotient, 3)
@@ -42,15 +42,15 @@ export const $$: getTestSet = () => {
 
     builder.add("unexpected division not null", {
         type: ['test', {
-            type: ['boolean', gpub.$r.divideWithRemainder({ numerator: 7, denominator: 0 })[0] === false]
+            type: ['boolean', g_pub.$r.divideWithRemainder({ numerator: 7, denominator: 0 })[0] === false]
         }]
     })
-    createTest("multiply - empty", gpub.$r.multiply([]), 1)
-    createTest("multiply - entries", gpub.$r.multiply([7, 6]), 42)
+    createTest("multiply - empty", g_pub.$r.multiply([]), 1)
+    createTest("multiply - entries", g_pub.$r.multiply([7, 6]), 42)
 
-    createTest("substract", gpub.$r.substract({ minuend: 42, subtrahend: 7 }), 35)
+    createTest("substract", g_pub.$r.substract({ minuend: 42, subtrahend: 7 }), 35)
 
-    createTest("max", gpub.$r.maxOrZero([42, 6, 8]), 42)
+    createTest("max", g_pub.$r.maxOrZero([42, 6, 8]), 42)
     //createTest("dictionaryMax", pub.$r.dictionaryMaxOrZero(pd.wrapRawDictionary({ "a": 42, "b": 6 })), 42)
     //createTest("arrayMax", pub.$r.arrayMaxOrZero(pr.wrapRawArray([42, 6, 8])), 42)
 
