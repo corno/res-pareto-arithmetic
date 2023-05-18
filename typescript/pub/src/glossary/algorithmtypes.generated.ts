bb8da2c66e911bb3e1a9d80319eb97f8e47d5fc0
corno <corno.astn.a@schraverus.com>
2023-05-18 15:38:42 +0200

    .

---
+++ typescript/pub/src/glossary/algorithmtypes.generated.ts
@@ -0,0 +1,63 @@
+import * as pt from 'pareto-core-types'
+
+import { T } from "./datatypes.generated"
+
+import * as g_common from "glo-pareto-common"
+
+export namespace ASYNC {
+    
+    export namespace I {}
+    
+    export namespace A {}
+}
+
+export namespace SYNC {
+    
+    export namespace A {
+        
+        
+        export namespace F {
+            export type Add = ($: T.NumberRange) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type ArrayMaxOrZero = ($: T.NumberArray) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type DictionaryMaxOrZero = ($: T.NumberDictionary) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type DivideWithRemainder = ($: T.DivisionData) => T.DivisionResult
+        }
+        
+        
+        export namespace F {
+            export type Increment = ($: g_common.T.Number) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type MaxOrZero = ($: T.NumberRange) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type Multiply = ($: T.NumberRange) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type Negate = ($: g_common.T.Number) => g_common.T.Number
+        }
+        
+        
+        export namespace F {
+            export type Substract = ($: T.SubstractData) => g_common.T.Number
+        }
+    }
+}
---
+++ typescript/pub/src/implementation.generated.ts
@@ -0,0 +1,22 @@
+import { API } from "./api.generated"
+import { $$ as iadd } from "./implementations/add.native"
+import { $$ as iarrayMaxOrZero } from "./implementations/arrayMaxOrZero.native"
+import { $$ as idictionaryMaxOrZero } from "./implementations/dictionaryMaxOrZero.native"
+import { $$ as idivideWithRemainder } from "./implementations/divideWithRemainder.native"
+import { $$ as iincrement } from "./implementations/increment.native"
+import { $$ as imaxOrZero } from "./implementations/maxOrZero.native"
+import { $$ as imultiply } from "./implementations/multiply.native"
+import { $$ as inegate } from "./implementations/negate.native"
+import { $$ as isubstract } from "./implementations/substract.native"
+
+export const $api: API = {
+    'add': iadd,
+    'arrayMaxOrZero': iarrayMaxOrZero,
+    'dictionaryMaxOrZero': idictionaryMaxOrZero,
+    'divideWithRemainder': idivideWithRemainder,
+    'increment': iincrement,
+    'maxOrZero': imaxOrZero,
+    'multiply': imultiply,
+    'negate': inegate,
+    'substract': isubstract,
+}
